386c6f3ccab4cb1a08e371f8f47e3b8724f51472
sunny <zhangdong@joysuch.com>
2020-09-01 15:37:07 +0800

    补

--- src/main/java/com/example/jenkinstest/JenkinstestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinstestApplication.java
@@ -2,12 +2,18 @@ package com.example.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinstestApplication {
+public class JenkinstestApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinstestApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JenkinstestApplication.class);
+    }
 }
