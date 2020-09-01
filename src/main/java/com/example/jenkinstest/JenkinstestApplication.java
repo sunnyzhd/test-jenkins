3df9ed746a8e2e7ee8d5c99df1a3e37e8b4ac457
sunny <zhangdong@joysuch.com>
2020-09-01 17:42:54 +0800

    打jar包找不到主类问题

--- src/main/java/com/example/jenkinstest/JenkinstestApplication.java
+++ src/main/java/com/example/jenkinstest/JenkinstestApplication.java
@@ -2,18 +2,17 @@ package com.example.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//extends SpringBootServletInitializer
 @SpringBootApplication
-public class JenkinstestApplication extends SpringBootServletInitializer {
+public class JenkinstestApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(JenkinstestApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(JenkinstestApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(JenkinstestApplication.class);
+//    }
 }
